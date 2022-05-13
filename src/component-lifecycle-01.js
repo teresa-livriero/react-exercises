a923c7680a8c7cbc90b08431600b26ba1c54964c
by-tess <livrierot@gmail.com>
2022-05-13 20:11:43 +0200

    push files

---
+++ src/component-lifecycle-01.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export class Counter extends React.Component {
+	state = {
+		count: 0,
+	}
+
+	componentDidMount() {
+		setInterval(() => {
+			this.setState((state) => {
+				return {
+					count: state.count + 1,
+				}
+			})
+		}, 1000)
+	}
+
+	render() {
+		return <h1>{this.state.count}</h1>
+	}
+}
