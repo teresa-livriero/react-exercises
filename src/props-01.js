0a6f6b538722ddf83e0690d9954402f13baa27a0
by-tess <livrierot@gmail.com>
2022-05-13 13:24:47 +0200

    push files

---
+++ src/props-01.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export class Welcome extends React.Component {
+	render() {
+		return <p>Hello, {this.props.name}!</p>
+	}
+}
+
+export class App extends React.Component {
+	render() {
+		return <Welcome name="Jimmy" />
+	}
+}
