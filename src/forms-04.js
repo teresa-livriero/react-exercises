cdff4b9557387fa0e5697d024867bb815b888d5a
by-tess <livrierot@gmail.com>
2022-05-14 14:58:35 +0200

    push files

---
+++ src/forms-04.js
@@ -0,0 +1,64 @@
+import React from 'react'
+
+export class Login extends React.Component {
+	state = {
+		username: '',
+		password: '',
+		remember: false,
+	}
+
+	handleChange = (event) => {
+		const name = event.target.name
+		const value = event.target.value
+		const type = event.target.type
+		const checked = event.target.checked
+
+		this.setState({
+			[name]: type === 'checkbox' ? checked : value,
+		})
+	}
+
+    handleClick = () => {
+		this.props.onLogin(this.state)
+	}
+
+	handleReset = () => {
+		this.setState({
+			username: '',
+			password: '',
+			remember: false,
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<input
+					name="username"
+					value={this.state.username}
+					onChange={this.handleChange}
+				/>
+				<input
+					name="password"
+					value={this.state.password}
+					onChange={this.handleChange}
+				/>
+				<input
+					name="remember"
+					type="checkbox"
+					checked={this.state.remember}
+					onChange={this.handleChange}
+				/>
+                <button
+					disabled={!this.state.username || !this.state.password}
+					onClick={this.handleClick}
+				>
+                    Login
+				</button>
+                <button onClick={this.handleReset}>
+					Reset
+				</button>
+			</div>
+		)
+	}
+}
