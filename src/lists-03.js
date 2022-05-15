bc5a0fab56d0fd8ec97b966c266486fb707cd74a
by-tess <livrierot@gmail.com>
2022-05-15 10:59:24 +0200

    push files

---
+++ src/lists-03.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export class TodoList extends React.Component {
+	state = {
+		todos: [],
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault()
+
+		const todo = event.target.elements.todo.value
+
+		this.setState((state) => {
+			return {
+				todos: [...state.todos, todo],
+			}
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<input name="todo" />
+					<button type="submit">Submit</button>
+				</form>
+
+				<ul>
+					{this.state.todos.map((todo, index) => (
+						<li key={index}>{todo}</li>
+					))}
+				</ul>
+			</div>
+		)
+	}
+}
