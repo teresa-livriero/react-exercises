d57504b3f4b0913a729d79dfde13093fa50cec2c
by-tess <livrierot@gmail.com>
2022-05-23 13:31:17 +0200

    push files

---
+++ src/redux-01.js
@@ -0,0 +1,36 @@
+const INCREMENT = 'COUNTER@INCREMENT'
+const DECREMENT = 'COUNTER@DECREMENT'
+const RESET = 'COUNTER@RESET'
+
+export function incrementCounter(increment = 1) {
+	return {
+		type: INCREMENT,
+		payload: increment,
+	}
+}
+
+export function decrementCounter(decrement = 1) {
+	return {
+		type: DECREMENT,
+		payload: decrement,
+	}
+}
+
+export function resetCounter() {
+	return {
+		type: RESET,
+	}
+}
+
+export function counterReducer(state = 0, action) {
+	switch (action.type) {
+		case INCREMENT:
+			return state + action.payload
+		case DECREMENT:
+			return state - action.payload
+		case RESET:
+			return 0
+		default:
+			return state
+	}
+}
